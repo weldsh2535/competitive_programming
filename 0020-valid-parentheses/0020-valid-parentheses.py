a770a42f2f9f189e8a64fa4ab54c1f9fa5704ae7
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2024-08-13 10:09:14 +0300

    Time: 40 ms (24.54%), Space: 16.4 MB (99.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stk = []
+        d = {'()', '[]', '{}'}
+        for c in s:
+            if c in '({[':
+                stk.append(c)
+            elif not stk or stk.pop() + c not in d:
+                return False
+        return not stk
+        
