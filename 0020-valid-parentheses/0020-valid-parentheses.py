241939e5689e5b494df6a79069c12871cc03666c
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2024-08-19 11:38:18 +0300

    Time: 32 ms (77.27%), Space: 16.6 MB (47.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,12 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        stk = []
-        d = {'()', '[]', '{}'}
-        for c in s:
-            if c in '({[':
-                stk.append(c)
-            elif not stk or stk.pop() + c not in d:
-                return False
-        return not stk
+        stack = []
+        
+        for ch in s:
+            if ch in '({[':
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                
+                character = stack.pop()
+                if character == "(":
+                    if ch != ")":
+                        return False
+                if character == "{":
+                    if ch != "}":
+                        return False
+                if character == "[":
+                    if ch != "]":
+                        return False
+                    
+        if stack:
+            return False
+        
+        return True
         
