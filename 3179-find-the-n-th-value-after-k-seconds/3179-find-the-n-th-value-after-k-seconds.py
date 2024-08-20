448a8afb593d2cc5c3ce13086bdef43397a92701
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2024-08-20 16:15:00 +0300

    Time: 4249 ms (51.42%), Space: 16.6 MB (79.43%) - LeetHub

--- 3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.py
+++ 3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.py
@@ -2,7 +2,7 @@ class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
         s = [1 for _ in range(n)]
         MOD = 10**9 + 7
-        for i in range(k):
+        for _ in range(k):
             for j in range(1,n):
                 s[j] = (s[j] + s[j-1]) % MOD
         return s[n-1]
