26a9a02d611c6c4516c530b2a2b93c0bf72cc606
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2025-05-07 11:08:54 +0300

    Create file

---
+++ 509-FibonacciNumber/version/python3/509-FibonacciNumber_20250507_110852.py
@@ -0,0 +1,8 @@
+# Last updated: 07/05/2025, 11:08:52
+class Solution:
+    def fib(self, n: int) -> int:
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
+        
