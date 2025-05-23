c6f0daed23200bce3f058c198790a116076707b5
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2025-05-23 23:33:53 +0300

    Create 115-DistinctSubsequences

---
+++ 115-DistinctSubsequences/version/typescript/115-DistinctSubsequences_20250523_233328.ts
@@ -0,0 +1,22 @@
+// Last updated: 23/05/2025, 23:33:28
+function numDistinct(s: string, t: string): number {
+    const m = s.length;
+    const n = t.length;
+    const dp: number[][] = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    for (let i = 0; i <= m; i++) {
+        dp[i][0] = 1;
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s[i - 1] === t[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[m][n];
+};
