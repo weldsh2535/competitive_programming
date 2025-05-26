9e5dd45e65fb29af880cc45b5ec45104ca272174
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2025-05-26 14:38:41 +0300

    Create 2677-ChunkArray

---
+++ 2677-ChunkArray/version/typescript/2677-ChunkArray_20250526_143837.ts
@@ -0,0 +1,11 @@
+// Last updated: 26/05/2025, 14:38:37
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+    const chunkedArr: any[][] = [];
+    for (let i = 0; i < arr.length; i += size) {
+        chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+};
