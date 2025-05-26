28af6e528c65c2bf9fb21d46ff3b5a2eecc8d864
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2025-05-26 14:37:23 +0300

    Create 2727-IsObjectEmpty

---
+++ 2727-IsObjectEmpty/version/typescript/2727-IsObjectEmpty_20250526_143719.ts
@@ -0,0 +1,13 @@
+// Last updated: 26/05/2025, 14:37:19
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else if (typeof obj === 'object' && obj !== null) {
+        return Object.keys(obj).length === 0;
+    } else {
+        return true;
+    }
+};
